7d4e61e529e34608771c6db2c5aafe27fbb249cb
Jacob Glueck <swimgiraffe435@gmail.com>
2016-07-20 08:01:03 -0400

    Switch to bootstrap theme

--- conf.py
+++ conf.py
@@ -1,3 +1,5 @@
+import sphinx_bootstrap_theme
+
 # -*- coding: utf-8 -*-
 #
 # Gentoo Setup Guide documentation build configuration file, created by
@@ -122,13 +124,26 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'nature'
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+# Theme options are theme-specific and customize the look and feel of a
+# theme further.
+html_theme_options = {
+    # Global TOC depth for "site" navbar tab. (Default: 1)
+    # Switching to -1 shows all levels.
+    'globaltoc_depth': -1,
+
+    # Bootswatch (http://bootswatch.com/) theme.
+    #
+    # Options are nothing (default) or the name of a valid theme
+    # such as "amelia" or "cosmo".
+    'bootswatch_theme': "readable",
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
